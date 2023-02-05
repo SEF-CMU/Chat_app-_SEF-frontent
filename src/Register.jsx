ea01de5d7dd272466075b8da4f9fee5988663f6b
VenantTumukunde <ventum11@gmail.com>
2023-02-05 18:01:55 +0200

    apply figma desgin

--- src/Register.jsx
+++ src/Register.jsx
@@ -5,6 +5,7 @@ export const Register = (props) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [phone, setPhone] = useState('');
     const [passConfirm, setPassConfirm] = useState('');
     
 
@@ -18,7 +19,7 @@ export const Register = (props) => {
         }
 
         axios.post('http://127.0.0.1:5000/api/v1/users/signup', registered)
-        .then(response => console.log(response.data)).then(props.onFormSwitch('login'));
+        .then(response => console.log(response)).then(props.onFormSwitch('login'));
         
         
 
@@ -29,17 +30,18 @@ export const Register = (props) => {
         <div className="auth-form-container">
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <label htmlFor="passwordConfirm">confirm password</label>
-            <input value={passConfirm} onChange={(e) => setPassConfirm(e.target.value)} type="password" placeholder="********" id="passwordConfirm" name="passwordConfirm" />
-            <button type="submit">Sign Up</button>
+            {/* <label htmlFor="name">Full name</label> */}
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Name" />
+            {/* <label htmlFor="email">email</label> */}
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Email" id="email" name="email" />
+            <input value={phone} onChange={(e) => setPhone(e.target.value)}type="text" placeholder="Phone Number" id="phone" name="phone" />
+            {/* <label htmlFor="password">password</label> */}
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
+            {/* <label htmlFor="passwordConfirm">confirm password</label> */}
+            <input value={passConfirm} onChange={(e) => setPassConfirm(e.target.value)} type="password" placeholder="Confirm Password" id="passwordConfirm" name="passwordConfirm" />
+            <button type="submit" className="sub-btn">Sign Up</button>
         </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login <font color="red">here</font></button>
     </div>
     )
 }
